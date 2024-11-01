cf1e48b8cd1aada64ab6f6327b930c39cbd058e2
Valentina <valya.valya288@gmail.com>
2024-11-01 20:55:16 +0300

    Diplom_1

--- src/test/java/praktikum/BunTest.java
+++ src/test/java/praktikum/BunTest.java
@@ -4,18 +4,21 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class BunTest {
-    private String name="sdc";
-    private float price= 27.5f;
-    Bun bun = new Bun(name, price);
 
     @Test
     public void getNameTest() {
+        String name="sdc";
+        float price= 27.5f;
+        Bun bun = new Bun(name, price);
         String result = bun.getName();
         assertEquals(name,result);
     }
 
     @Test
     public void getPriceTest() {
+        String name="sdc";
+        float price= 27.5f;
+        Bun bun = new Bun(name, price);
         float result = bun.getPrice();
         assertEquals(price,result, 0);
     }
